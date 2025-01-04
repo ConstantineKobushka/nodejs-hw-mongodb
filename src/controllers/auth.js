b12fe1a2bcea44a05a0d442a1aca3773fae1ca94
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-01-04 17:22:02 +0200

    added refresh and logout

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,18 +1,6 @@
 import * as authServices from '../services/auth.js';
 
-export const registerController = async (req, res) => {
-  const newUser = await authServices.registerUser(req.body);
-
-  res.status(201).json({
-    status: 201,
-    message: 'Successfully registered a user!',
-    data: { name: newUser.name, email: newUser.email },
-  });
-};
-
-export const loginController = async (req, res) => {
-  const session = await authServices.loginUser(req.body);
-
+const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expires: session.refreshTokenValidUntil,
@@ -22,6 +10,28 @@ export const loginController = async (req, res) => {
     httpOnly: true,
     expires: session.refreshTokenValidUntil,
   });
+};
+
+export const registerController = async (req, res) => {
+  const newUser = await authServices.registerUser(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully registered a user!',
+    data: {
+      name: newUser.name,
+      email: newUser.email,
+      _id: newUser.id,
+      createdAt: newUser.createdAt,
+      updatedAt: newUser.updatedAt,
+    },
+  });
+};
+
+export const loginController = async (req, res) => {
+  const session = await authServices.loginUser(req.body);
+
+  setupSession(res, session);
 
   res.json({
     status: 200,
@@ -30,4 +40,30 @@ export const loginController = async (req, res) => {
       accessToken: session.accessToken,
     },
   });
+};
+
+export const refreshTokenController = async (req, res) => {
+  const { refreshToken, sessionId } = req.cookies;
+  const session = await authServices.refreshToken({ refreshToken, sessionId });
+
+  setupSession(res, session);
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+export const logoutController = async (req, res) => {
+  if (req.cookies.sessionId) {
+    await authServices.logout(req.cookies.sessionId);
+  }
+
+  res.clearCookie('refreshToken');
+  res.clearCookie('sessionId');
+
+  res.status(204).send();
 };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import contactsRouter from './routers/contactsRouter.js';
 import authRouter from './routers/auth.js';
@@ -15,6 +16,7 @@ export const setupServer = () => {
 
   app.use(cors());
   app.use(express.json());
+  app.use(cookieParser());
   app.use(logger);
 
   app.use('/auth', authRouter);
