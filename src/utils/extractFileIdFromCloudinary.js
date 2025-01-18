9a37ce3152cf703fcd36e028c97b7a66f51be2e0
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-01-18 11:02:46 +0200

    fixed cod

--- src/utils/extractFileIdFromCloudinary.js
+++ src/utils/extractFileIdFromCloudinary.js
@@ -1,12 +1,4 @@
-// export const extractFileIdFromCloudinary = (url) => {
-//   const parts = url.split('/');
-//   const fileName = parts[parts.length - 1];
-//   return `avatars/${fileName.split('.')[0]}`;
-// };
-
 export const extractFileIdFromCloudinary = (url) => {
-  const parts = url.split('/');
-  const fileName = parts[parts.length - 1].split('.')[0];
-  const folderName = parts[parts.length - 2];
-  return `${folderName}/${fileName}`;
+  const fileId = url.split('/').slice(-2).join('/').split('.')[0];
+  return fileId;
 };
