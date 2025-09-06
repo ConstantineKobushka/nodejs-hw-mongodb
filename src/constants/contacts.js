032f799a15939227c872aae976758db126736399
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-09-06 14:01:14 +0300

    crear main

--- src/constants/contacts.js
+++
@@ -1,1 +0,0 @@
-export const typeList = ['personal', 'home', 'work'];
--- src/utils/extractFileIdFromCloudinary.js
+++
@@ -1,4 +0,0 @@
-export const extractFileIdFromCloudinary = (url) => {
-  const fileId = url.split('/').slice(-2).join('/').split('.')[0];
-  return fileId;
-};
